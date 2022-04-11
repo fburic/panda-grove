d8f4c41b22629ceb2f5a60abd0afdd1eaa6591e0
Filip Buric <29886463+fburic@users.noreply.github.com>
2022-04-11 21:14:57 +0200

    fix: Revert to hardcoded version for now

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
-import grove
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="panda-grove",
-    version=grove.__version__,
+    version='0.1.1',
     author="Filip Buric",
     author_email="",
     description="Convenience wrappers for Pandas DataFrame collections and operations",
